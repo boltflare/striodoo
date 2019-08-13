838946bfb90a76bba9cb12eb26efe8354687e027
Luis <luis.dominguez19@yahoo.es>
2019-08-13 16:32:18 -0500

    hs_chartfield added
    El modulo hs_chart_field fue anexado al proyecto

---
+++ hs_chart_field/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "ChartField",
+
+	'summary': """ChartField""",
+
+	'description': """
+		Agrega la propiedad ChartField
+	""",
+
+	'author': "HS Consulting S.A.",
+	'website': "http://www.hconsul.com/odoo/",
+	'maintainer': 'Luis Dominguez',
+
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+	# for the full list
+	'category': 'Tools',
+	'version': '1.0',
+	'license': 'LGPL-3',
+
+	# any module necessary for this one to work correctly
+	'depends': ['base', 'account'],
+
+	# any external library necessary for this one to work correctly
+	'external_dependencies': {
+		'python': [],
+	},
+
+	# always loaded
+	'data': [
+		'views/res_partner.xml',
+		'views/account_account.xml',
+	],
+	'installable': True,
+	'auto_install': True,
+	'application': False,
+}
---
+++ hs_chart_field/models/res_partner.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions
+
+class ResPartnerInherit(models.Model):
+	_inherit = 'res.partner'
+
+	stri_fund = fields.Integer("Fund Code")
+	stri_budget = fields.Integer("Budget Ref")
+	stri_desig = fields.Integer("Designated Code")
+	stri_dept = fields.Integer("Dept ID")
+	stri_account = fields.Integer("Account")
+	stri_class = fields.Integer("Class Field")
+	stri_program = fields.Integer("Program Code")
+
+	stri_project = fields.Char("Project ID")
+	stri_activity = fields.Char("Activity Code")
+	stri_type = fields.Selection([("GLE", "GLE"), ("GLR", "GLR")], "Type")
+
+	stri_chartfield = fields.Char(compute="_computed_chartfield", 
+		string="Char Field")
+
+
+	@api.depends('stri_fund', 'stri_budget', 'stri_desig', 'stri_dept',
+		'stri_account', 'stri_class', 'stri_program', 'stri_project',
+		'stri_activity', 'stri_type')
+	def _computed_chartfield(self):
+
+		resp = str(self.stri_fund) if self.stri_fund else ""
+
+		resp = resp + "," + str(self.stri_budget) if self.stri_budget \
+			else resp + ","
+		
+		resp = resp + "," + str(self.stri_desig) if self.stri_desig \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_dept) if self.stri_dept \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_account) if self.stri_account \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_class) if self.stri_class \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_program) if self.stri_program \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_project) if self.stri_project \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_activity) if self.stri_activity \
+			else resp + ","
+
+		resp = resp + "," + str(self.stri_type) if self.stri_type \
+			else resp + ","
+
+		self.stri_chartfield = resp
+
+		"""
+		result = ""
+		if self.stri_fund:
+			result = result + str(self.stri_fund)
+		if self.stri_budget:
+			result = result + "," + str(self.stri_budget)
+		if self.stri_desig:
+			result = result + "," + str(self.stri_desig)
+		if self.stri_dept:
+			result = result + "," + str(self.stri_dept)
+		if self.stri_account:
+			result = result + "," + str(self.stri_account)
+
+		if self.stri_class:
+			result = result + "," + str(self.stri_class)
+		if self.stri_program:
+			result = result + "," + str(self.stri_program)
+		if self.stri_project:
+			result = result + "," + self.stri_project
+		if self.stri_activity:
+			result = result + "," + self.stri_activity
+		if self.stri_type:
+			result = result + "," + self.stri_type
+		
+		self.stri_chartfield = result
+		"""
