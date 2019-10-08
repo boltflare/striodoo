7ded346e093fcdbb81d993c2027d63a19f4c0100
Ceila Hernandez <chernandez@hermecsolutions.com>
2019-10-08 05:11:11 -0500

    Update main.py

--- odoo_microsoft_account/controllers/main.py
+++ odoo_microsoft_account/controllers/main.py
@@ -95,7 +95,7 @@ class OAuthController(http.Controller):
         try:
             conn = httplib.HTTPSConnection(provider.data_endpoint)
             _logger.info("conn " + str(conn))
-            conn.request("GET", "/userinfo", "", {
+            conn.request("GET", "/adfs/userinfo", "", {
                 'Authorization': access_token,
                 'Accept': 'application/json'
             })
