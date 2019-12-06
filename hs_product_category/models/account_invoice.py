8e94e87369b109f40686bb21dd96825bb127136e
Luis <luis.dominguez19@yahoo.es>
2019-12-06 14:12:44 -0500

    test

--- hs_product_category/models/account_invoice.py
+++ hs_product_category/models/account_invoice.py
@@ -16,11 +16,11 @@ class accountInvoiceInherit2(models.Model):
 		try:
 			for invoice_line in self.invoice_line_ids:
 				product = invoice_line.product_id
-				_logger.info(product.name)
-				journals = self.sudo().env["account.journal"].filtered(lambda l: l.department_ids == product.categ_id)
+				_logger.info("El producto encontrado es: " + product.name)
+				journals = self.sudo().env["account.journal"].search([('type', '=', 'sale')])
 				for item in journals:
 					_logger.info("los journals encontrados son: " + item.name)	
-				journal = journals.search([('type', '=', 'sale')])
+				journal = journals.filtered(lambda l: l.department_ids == product.categ_id)
 				self.journal_id = journal
 				_logger.info("El journal encontrado es: " + journal.name)
 				break
