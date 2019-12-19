8996f2ecb12d527ebe2bb55060842c6a45e46d30
Luis <luis.dominguez19@yahoo.es>
2019-12-19 14:51:11 -0500

    fixed

--- hs_custom_reports/reports/items_funds.py
+++ hs_custom_reports/reports/items_funds.py
@@ -13,9 +13,10 @@ class ReportItemFund(models.AbstractModel):
 	def _get_report_values(self, docids, data=None):
 		report_name = "hs_custom_reports.item_fund_template"
 		report = self.env["ir.actions.report"]._get_report_from_name(report_name)
+		docs = self.env["account.payment"].browse(docids)
 		return {
 			'doc_ids' : docids,
 			'doc_model':report.model,
-			'docs' : self.env['report_type'] if data else '',
+			'docs' : docs,
 		}
 
