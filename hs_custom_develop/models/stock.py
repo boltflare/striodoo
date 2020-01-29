adbb848e8c5da86a2fb3f6c3cd7a4771ce941e26
Luis <luis.dominguez19@yahoo.es>
2020-01-29 14:33:21 -0500

    update

--- hs_custom_develop/models/stock.py
+++ hs_custom_develop/models/stock.py
@@ -9,16 +9,16 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
 	_inherit = "stock.picking"
 
-	enable_validate = fields.Boolean(string="Enable validate action", 
-		compute="compute_enable_validate")
+	enable_button_validate = fields.Boolean(string="Enable validate action", 
+		compute="compute_enable_button_validate")
 
 
-	def compute_enable_validate(self):
+	def compute_enable_button_validate(self):
 		for stock in self:
 			if stock.sale_id:
 				sale = stock.sale_id
 				has_invoice = False if not sale.invoice_ids else True
 				if sale.required_invoice == True and has_invoice == True:
-					stock.enable_validate = True
+					stock.enable_button_validate = True
 				else:
-					stock.enable_validate = False
+					stock.enable_button_validate = False
