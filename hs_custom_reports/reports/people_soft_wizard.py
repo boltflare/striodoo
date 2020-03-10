372b832d291eddba3e80eeaa0b665266f7eae159
Luis <luis.dominguez19@yahoo.es>
2020-03-10 17:14:45 -0500

    fixed

--- hs_custom_reports/reports/people_soft_wizard.py
+++ hs_custom_reports/reports/people_soft_wizard.py
@@ -3,7 +3,7 @@
 
 from odoo import models, fields, api, _
 
-class PeopleSoftWizard(models.Model):
+class PeopleSoftWizard(models.TransientModel):
 	_name = 'account.people.soft.wizard'
 	_description = 'Final People Soft Wizzard'
 
