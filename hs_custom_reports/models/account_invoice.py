3bc2ad81f9b23d8e4dd67f75c31ba8938c093812
Luis <luis.dominguez19@yahoo.es>
2020-03-19 13:39:48 -0500

    remove code

--- hs_custom_reports/models/account_invoice.py
+++ hs_custom_reports/models/account_invoice.py
@@ -8,21 +8,24 @@ from odoo.exceptions import ValidationError
 class AccountInvoiceInherit(models.Model):
 	_inherit = "account.invoice"
 	people_soft_registered = fields.Boolean("PeopleSoft Registered", default=False)
+	"""
 	people_soft_categ = fields.Selection(string="People Soft Category", 
 		selection=[
 			('customer_bci',  'Customer Account BCI'),
 			('customer_stri', 'Customer Account STRI'),
 			('strifund',      'STRIFUND'),
 		])
+	"""
 
 
-
+	"""
 	@api.multi
 	def action_invoice_open(self):
 
 		for invoice in self:
 			if invoice.partner_id.customer_type == 'fund':
 				invoice.people_soft_categ = 'strifund'
+			
 			elif "stri" in str(invoice.journal_id).lower():
 				invoice.people_soft_categ = "customer_stri"
 			elif "bci" in str(invoice.journal_id).lower():
@@ -32,4 +35,5 @@ class AccountInvoiceInherit(models.Model):
 					"para el reporte de People Soft."))
 		
 		return super(AccountInvoiceInherit, self).action_invoice_open()
+	"""
 		
