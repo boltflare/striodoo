46b4d103487a902cd31f02bb5ce7a5630ab6fd27
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-14 13:44:00 -0500

    Instalando prueba de vista

---
+++ hs_custom_invoice_view/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# 
+
+{
+	'name': 'HS Custom Invoice View',
+	'version': '1.0',
+	'summary':'Vista de Integracion para Meal Card',
+	'category': 'Tool',
+	'depends': ['base', 'account'],
+	'description': """
+		En este modulo se creara una vista de prueba para integracion de Meal Card de HS Consulting para STRI
+	""",
+
+	'author': 'Hermec Consulting, S.A.',
+    'maintainer':'Ceila Hernandez',
+
+	'data': [
+		'views/invoice_view.xml',
+
+	],
+	# 'qweb': [
+	# 	# 'static/src/xml/pos_hide_tax.xml'
+	# ],
+		
+	'installable': True,
+	'auto_install': False,
+}
---
+++ hs_custom_invoice_view/models/invoice_view.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions
+
+class InvoiceView(models.Model):
+	_name= 'invoice.view'
+	# _inherit = 'account.invoice'
+	# _description = 'Account Invoice View'
+
+	# partner_id = fields.Many2one('res.partner', string='Partner', change_default=True,
+    #     readonly=True, states={'draft': [('readonly', False)]},
+    #     track_visibility='always', help="You can find a contact by its Name, TIN, Email or Internal Reference.")
+	# number = fields.Char(related='move_id.name', store=True, readonly=True, copy=False)
+	# quantity = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'),
+    #     required=True, default=1)
+	# date_invoice = fields.Date(string='Invoice Date',
+    #     readonly=True, states={'draft': [('readonly', False)]}, index=True,
+    #     help="Keep empty to use the current date", copy=False)
+	#  amount_total = fields.Monetary(string='Total',
+    #     store=True, readonly=True, compute='_compute_amount')
+	# name = fields.Text(string='Description', required=True)
+	
+	number = fields.Char(string='Invoice #', required = True)
+	quantity = fields.Float(string='Quantity', required = True)
+	date = fields.Date(string='Invoice Date')
+	product_id = fields.Integer("Item Code")
+	partner_id = fields.Char(string='Customer', required = True)
+	total = fields.Monetary(string='Total')
+	note = fields.Text(string='Description', required=True)
+
+
