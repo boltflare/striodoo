43bda89386c374ab3361c2306fa492cd25e6b857
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-15 14:01:55 -0500

    Update 13

--- account_invoice_pricelist/models/account_invoice.py
+++ account_invoice_pricelist/models/account_invoice.py
@@ -79,34 +79,33 @@ class AccountInvoiceLine(models.Model):
         for line in self.filtered(lambda r: r.invoice_id.state == 'draft'):
             line._onchange_product_id_account_invoice_pricelist()
 
-
-    # @api.onchange('product_id','pricelist_id')
-    # def _onchange_prices_from_pricelist(self):
-    #     # result = super(AccountInvoice, self)._onchange_partner_id()
-    #     if self.partner_id and self.invoice_id.pricelist_id
-    #             and self.invoice_line_ids.product_id
-    #         self.pricelist_id = self.partner_id.property_product_pricelist
-    #     # return result
-    
-    # @api.multi
-    # def update_prices_from_pricelist(self):
-    #     """update current prices from pricelist"""
-    #     for inv in self.filtered(lambda r: r.state == 'draft'):
-    #         inv.invoice_line_ids.filtered('product_id').update_from_pricelist()
-    #     self.filtered(lambda r: r.state == 'draft').compute_taxes()
-
     @api.multi
+    def update_prices_from_pricelist(self):
+        for inv in self.filtered(lambda r: r.state == 'draft'):
+                inv.invoice_line_ids.filtered('product_id').update_from_pricelist()
+        self.filtered(lambda r: r.state == 'draft').compute_taxes()*-
 
-    @api.onchange('pricelist_id')
-    def product_price_change(self):
+    # @api.onchange('pricelist_id')
+    # def _onchange_price_account_invoice_pricelist(self):
+    #     result = super(AccountInvoiceLine, self)._onchange_invoice_pricelist()
+    #     if self.pricelist_id and self.type in ('out_invoice', 'out_refund')\
+    #             and self.partner_id.property_product_pricelist:
+    #         self.pricelist_id = self.partner_id.property_product_pricelist
+    #     return result
+   
+   
+    # @api.multi
 
-        res = super(AccountInvoiceLine, self).product_price_change()
+    # @api.onchange('pricelist_id')
+    # def product_price_change(self):
 
-        for line in self:
+    #     res = super(AccountInvoiceLine, self).product_price_change()
 
-            line.invoice_line_ids.filtered('product_id').update_from_pricelist()
+    #     for line in self:
+
+    #         line.invoice_line_ids.filtered('product_id').update_from_pricelist()
 
 
-            # line.price_unit = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, self.tax_id)
+    #         # line.price_unit = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, self.tax_id)
 
-        return res
+    #     return res
