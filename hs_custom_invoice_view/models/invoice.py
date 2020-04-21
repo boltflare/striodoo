66f400d3e4c4e2415ba6a068e461f339ab9f1d24
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-21 11:47:38 -0500

    Update hs_custom_invoice_view

---
+++ hs_custom_invoice_view/models/invoice.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions
+
+class InvoiceInherit2(models.Model):
+	_inherit = 'account.invoice'
+    # _inherit = ['todo.task', 'mail.thread']
+
+	# is_fund = fields.Boolean(string="Is Fund")
+
+	note =  fields.Char(string='Description')
+
+
+    # type = fields.Selection([
+    #     ('consu', 'Consumable'),
+    #     ('service', 'Service')], string='Product Type', default='consu', required=True,
+    #     help='A storable product is a product for which you manage stock. The Inventory app has to be installed.\n'
+    #          'A consumable product is a product for which stock is not managed.\n'
+    #          'A service is a non-material product you provide.')
