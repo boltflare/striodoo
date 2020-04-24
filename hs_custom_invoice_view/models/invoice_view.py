b6e7c19f6bef0b5d049719e334e3248b028086cd
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-24 14:59:54 -0500

    Update

--- hs_custom_invoice_view/models/invoice_view.py
+++ hs_custom_invoice_view/models/invoice_view.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 class InvoiceView(models.Model):
 	_name= 'invoice.view'
-	#_inherit = 'account.invoice'
+	_inherit = 'account.invoice'
 	# _description = 'Account Invoice View'
 
 	# partner_id = fields.Many2one('res.partner', string='Partner', change_default=True,
@@ -32,20 +32,6 @@ class InvoiceView(models.Model):
 	note = fields.Char(string='Description')
 
 	
-	# item_food = fields.Boolean(string="Is item food?", compute="_item_type", default=False)
-
-	# @api.depends('partner_id')
-	# def _item_type(self):
-	# 	# self.customer_is_fund = True if self.partner_id.customer_type == 'fund' else False
-	# 	for invoice in self:
-	# 		customer = invoice.partner_id.customer
-	# 		invoice.item_food = True if customer == True else False
 	
-	# @api.depends('item_type')
-	# def _item_type(self):
-	# 	# self.customer_is_fund = True if self.partner_id.customer_type == 'fund' else False
-	# 	for invoice in self:
-	# 		customer_type = invoice.partner_id.customer_type
-	# 		invoice.customer_is_fund = True if customer_type == 'fund' else False
 
 	
