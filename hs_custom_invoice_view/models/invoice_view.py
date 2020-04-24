346b12cdbff0d4f7a3432f058dbac50a141d8948
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-24 15:35:36 -0500

    Update22

--- hs_custom_invoice_view/models/invoice_view.py
+++ hs_custom_invoice_view/models/invoice_view.py
@@ -21,9 +21,9 @@ class InvoiceView(models.Model):
 	# name = fields.Text(string='Description', required=True)
 
 	# class_code = fields.Many2one("class.code", "Class Code")
-	hs_number = fields.Char(string='Invoice #', related='invoice_id.number', store=True, readonly=False)
+	hs_number = fields.Char(related='invoice_id.number', store=True, readonly=False)
 	hs_quantity = fields.Float(string='Quantity')
-	hs_date = fields.Date(string='Invoice Date')
+	hs_date = fields.Date(string='Invoice Date', related='invoice_id.date_invoice', store=True, readonly=False)
 	hs_product_id = fields.Char("Item Code") #debo cambiarlo que sea tipo char
 	# item = fields.Char (related='item_type.item')
 	hs_item= fields.Many2one("product.template", "Item Type")
