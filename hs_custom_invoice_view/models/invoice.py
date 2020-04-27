07afec3353d3b14346edf4efb444a50a478cdeee
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-27 12:40:48 -0500

    Update

--- hs_custom_invoice_view/models/invoice.py
+++ hs_custom_invoice_view/models/invoice.py
@@ -4,16 +4,15 @@ from odoo import models, fields, api, exceptions
 
 class InvoiceInherit2(models.Model):
 	_inherit = 'account.invoice'
-    # _inherit = ['todo.task', 'mail.thread']
+	# _inherit = ['todo.task', 'mail.thread']
 
 	# is_fund = fields.Boolean(string="Is Fund")
 
 	note =  fields.Char(string='Description')
 
-    
-    @api.multi
-    def update_status_meal_card(self):
-        lines = self.env['account.invoice.line'].search([('invoice_id', '=', self.id)])
-        if lines:
-            lines.write({'hs_state':self.state})
-    
+	@api.multi
+	def update_status_meal_card(self):
+		lines = self.env['account.invoice.line'].search([('invoice_id', '=', self.id)])
+		if lines:
+			lines.write({'hs_state':self.state})
+	
