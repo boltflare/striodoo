e1498c893069a33c8684fa1726adcd3c5c078bae
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-28 15:34:32 -0500

    Update invoice_view.py

--- hs_custom_invoice_view/models/invoice_view.py
+++ hs_custom_invoice_view/models/invoice_view.py
@@ -11,7 +11,7 @@ class InvoiceView(models.Model):
 	hs_number = fields.Char(compute='_compute_hs_number',string='Invoice #', store=True)
 	# hs_quantity = fields.Float(string='Quantity', related='invoice_id.quantity', store=True)
 	hs_date = fields.Date(string='Invoice Date', related='invoice_id.date_invoice', store=True, readonly=False)
-	hs_product_id = fields.Char(string='Item Code', related='invoice_line_ids.product_id', store=True) #debo cambiarlo que sea tipo char
+	hs_product_id = fields.Char(string='Item Code', related='product_id.default_code', store=True) #debo cambiarlo que sea tipo char
 	# item = fields.Char (related='item_type.item')
 	hs_item= fields.Selection(string= "Item Type", related='product_id.item_type')
 	hs_partner_id = fields.Char(compute='_compute_hs_partner_id', string='Customer', store=True)
@@ -41,10 +41,8 @@ class InvoiceView(models.Model):
 		if self.invoice_id.move_id:
 			self.hs_number = self.invoice_id.move_id.name
 		self.hs_date = self.invoice_id.date_invoice
-		if self.invoice_line_ids:
-			self.hs_product_id = self.invoice_line_ids.product_id
 		if self.product_id:
-			#self.hs_product_id = self.product_id.default_code
+			self.hs_product_id = self.product_id.default_code
 			self.hs_item = self.product_id.item_type	
 		self.hs_partner_id = self.invoice_id.partner_id.name
 		self.hs_note = self.invoice_id.note
