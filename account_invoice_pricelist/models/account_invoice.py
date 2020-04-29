a74a325efbfde48b9257cbdc4eb657cc0ac33e79
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-04-29 11:21:08 -0500

    Update account invoice

--- account_invoice_pricelist/models/account_invoice.py
+++ account_invoice_pricelist/models/account_invoice.py
@@ -29,7 +29,7 @@ class AccountInvoice(models.Model):
             inv.invoice_line_ids.filtered('product_id').update_from_pricelist()
         self.filtered(lambda r: r.state == 'draft').compute_taxes()
         # if self.bool_field:
-        self.bool_field = True #Esto permite que cuando haga click en el boton se habilite Validate
+        self.bool_field = True #Esto permite que cuando haga click en el boton, se habilite Validate
     
     @api.model
     def _prepare_refund(self, invoice, date_invoice=None, date=None,
