4a43b60c2c18ed73f436038e3749022be1d12b1c
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-11 11:25:48 -0500

    Install invoicees report test

---
+++ deltatech_invoice_report/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2020 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Invoice Report",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+    "category": "Accounting",
+    "depends": ['account','product'],
+
+    "data": [
+        'report/invoice_report_view.xml',
+        'views/product_view.xml',
+        'wizard/product_without_move_view.xml'
+    ],
+    "images": ['images/main_screenshot.png'],
+    "installable": True,
+}
