025f9a9c586179c305e5062f737c044e1a415d68
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-18 12:13:13 -0500

    update report

--- account_invoice_line_report/models/invoice_report.py
+++ account_invoice_line_report/models/invoice_report.py
@@ -8,12 +8,28 @@ class InvoiceReport(models.Model):
 	# _description = 'Account Invoice View'
 
 	# class_code = fields.Many2one("class.code", "Class Code")
-	property_account_income_id = fields.Many2one('account.account', string='Department', readonly=True)
+	property_account_income_id = fields.Many2one('account.account', string='Income Account', readonly=True)
 	# hs_quantity = fields.Float(string='Quantity', related='invoice_id.quantity', store=True)
-	stri_chartfield = fields.Char('account.account', string='Fund', readonly=True)
+	stri_chartfield = fields.Char('account.account', string='Char Field', readonly=True)
 
+	_depends = {
+		'account.account': [
+			'property_account_income_id', 'stri_chartfield', 
+		],
+	}
+	def _select(self):
+		select_str = """
+			SELECT sub.property_account_income_id, sub.stri_chartfield
+		"""
+		return select_str
 
-	
+	def _sub_select(self):
+		select_str = """
+				SELECT aa.property_account_income_id,
+					aa.stri_chartfield
+		"""
+		return select_str
+
 	
 	# @api.depends('invoice_id.state')
 	# def _get_default_state(self):
