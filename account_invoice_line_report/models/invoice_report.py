41d06c8f33a98ddf92da363082918cc44c1a6e6e
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-18 10:54:47 -0500

    update invoice_report.py

--- account_invoice_line_report/models/invoice_report.py
+++ account_invoice_line_report/models/invoice_report.py
@@ -8,7 +8,7 @@ class InvoiceReport(models.Model):
 	# _description = 'Account Invoice View'
 
 	# class_code = fields.Many2one("class.code", "Class Code")
-    property_account_income_id = fields.Many2one('account.account', string='Department', readonly=True)
+	property_account_income_id = fields.Many2one('account.account', string='Department', readonly=True)
 	# hs_quantity = fields.Float(string='Quantity', related='invoice_id.quantity', store=True)
 	stri_chartfield = fields.Char('account.account', string='Fund', readonly=True)
 
