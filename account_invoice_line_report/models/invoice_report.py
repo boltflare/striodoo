47874bce2f9d6e917d35271082cdb8ef0f1c99d4
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-20 07:35:07 -0500

    Update invoice_report.py

--- account_invoice_line_report/models/invoice_report.py
+++ account_invoice_line_report/models/invoice_report.py
@@ -8,11 +8,17 @@ class InvoiceReport(models.Model):
 	# _description = 'Account Invoice View'
 
 	# class_code = fields.Many2one("class.code", "Class Code")
-	property_account_income_id = fields.Many2one('account.account', string='Income Account', readonly=True)
+	property_account_income_id = fields.Many2one('account.account', string='Income Account', compute='_compute_hs_income', readonly=True, store=True)
 	# hs_quantity = fields.Float(string='Quantity', related='invoice_id.quantity', store=True)
 	stri_chartfield = fields.Char('account.account', string='Char Field', readonly=True)
 	# chartfield = fields.Char(string='ChartField', related='id.stri_chartfield', store=True)
 
+	@api.depends('invoice_id', 'product_id')
+	def _compute_hs_income(self):
+		for item in self:
+			line = self.env['account.invoice.line'].search([('invoice_id', '=', item.invoice_id.id),('product_id', '=', item.product_id.id)], limit=1)
+			item.property_account_income_id = line.account_id
+			
 	# _depends = {
 	# 	'account.account': [
 	# 		'property_account_income_id', 'stri_chartfield', 
