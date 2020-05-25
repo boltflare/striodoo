70c5a5077e6a1749d6cf5766760524deae71538d
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-25 14:51:36 -0500

    update

--- account_invoice_line_report/models/invoice_report.py
+++ account_invoice_line_report/models/invoice_report.py
@@ -16,6 +16,13 @@ class InvoiceReport(models.Model):
 	# test API
 	# logging.info(str(api.execute('/api')))
 	# logging.info(str(api.execute('/api/user')))
+
+	def action_muki_connect(self):
+		api = library.RestAPI()
+		api.authenticate()
+		logging.info(str(api.execute('/api')))
+		logging.info(str(api.execute('/api/user')))
+
 	# hs_fund = fields.Char(string='Fund Code', related='invoice_id.stri_fund', store=True)
 	# hs_budget = fields.Char(string='Budget Reference', related='invoice_id.stri_budget', store=True)
 	# hs_desig = fields.Char(string='Designated Code', related='invoice_id.stri_desig', store=True)
