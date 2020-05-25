2d56a13b1dc20d1b44206a43a217b981bc8f5d32
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-05-25 16:00:05 -0500

    123

--- account_invoice_line_report/models/invoice_report.py
+++ account_invoice_line_report/models/invoice_report.py
@@ -15,4 +15,4 @@ class InvoiceReport(models.Model):
         api = library.RestAPI()
         api.authenticate()
         # logging.info(str(api.execute('/api')))
-        logging.info('prueba REST API' + str(api.execute('/api/read/account.invoice.report?ids='+ str(self.ids))))
+        logging.info('prueba REST API' + str(api.execute('/api/read/account.invoice.report?ids=%5B375%5D&fields=%27number%27%2C%27id%27%2C%27account_line_id%27')))
