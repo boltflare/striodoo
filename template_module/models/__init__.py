2922bbd66a2f8c6869066ce2d40d4d4a061f5c71
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-07-06 14:05:31 -0500

    [add] module test API

---
+++ template_module/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import muki_rest
+from . import res_partner
---
+++ template_module/models/res_partner.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+from . import library
+import json
+from odoo import models, fields, api
+
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartnerInherit(models.Model):
+	_inherit = "res.partner"
+
+	def action_muki_connect(self):
+		# init API
+		api = library.RestAPI()
+		api.authenticate()
+
+		
+		# test API
+		logging.info(str(api.execute('/api')))
+		logging.info(str(api.execute('/api/user')))
+
+		data = {
+			'model': "account.invoice",
+			'domain': json.dumps([['type', '=', "out_invoice"], ['state', '!=', 'draft']]),
+			'fields': json.dumps(['number', 'amount_total']),
+		}
+		response = api.execute('/api/search_read', data=data)
+		for entry in response:
+			number = entry.get('number')
+			total = entry.get('amount_total')
+			self.env["muki.rest"].create({'name':number,'amount':total})
+		logging.info(str(response))
+
+
+		"""
+		# sampel query
+		data = {
+			'model': "res.partner",
+			'domain': json.dumps([['parent_id.name', '=', "Azure Interior"]]),
+			'fields': json.dumps(['name', 'image_small']),
+		}
+		response = api.execute('/api/search_read', data=data)
+		for entry in response:
+			entry['image_small'] = entry.get('image_small')[:5] + "..."
+		logging.info(str(response))
+
+		# check customer
+		data = {
+			'model': "res.partner",
+			'domain': json.dumps([['name', '=', "Sample Customer"]]),
+			'limit': 1
+		}
+		response = api.execute('/api/search', data=data)
+		customer = next(iter(response), False)
+
+		# create customer
+		if not customer:
+			values = {
+				'name': "Sample Customer",
+			}
+			data = {
+				'model': "res.partner",
+				'values': json.dumps(values),
+			}
+			response = api.execute('/api/create', type="POST", data=data)
+			customer = next(iter(response))
+
+		# create product
+		values = {
+			'name': "Sample Product",
+		}
+		data = {
+			'model': "product.template",
+			'values': json.dumps(values),
+		}
+		response = api.execute('/api/create', type="POST", data=data)
+		product = next(iter(response))
+
+		# create order
+		values = {
+			'partner_id': customer,
+			'state': 'sale',
+			'order_line': [(0, 0, {'product_id': product})],
+		}
+		data = {
+			'model': "sale.order",
+			'values': json.dumps(values),
+		}
+		response = api.execute('/api/create', type="POST", data=data)
+		order = next(iter(response))
+		"""
+
+
