ae11fa8e836fec091a30665abf555f5a7c2f0552
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-05 12:30:47 -0500

    test create from wizard

--- template_module/wizard/create_cutomer.py
+++ template_module/wizard/create_cutomer.py
@@ -6,15 +6,36 @@ class CreateCustomer(models.TransientModel):
     _description = 'Create a new visitor on customers'
     
     visitor_name = fields.Char("Name")
-    # state = fields.Selection([
-    #     ('draft', 'Quotation'),
-    #     ('sent', 'Quotation Sent'),
-    #     ('sale', 'Sales Order'),
-    #     ('done', 'Locked'),
-    #     ('cancel', 'Cancelled'),
-    # ], string = 'Status')
     
+    # def create_visitor(self):
+    #     active_ids = self._context.get('active_ids', []) or []
+    #     for record in self.env['muki.rest'].browse(active_ids):
+    #         record.visitor_name = self.visitor_name
+            
+    #Funcion para obtener los registros seleccionados
+    def _get_visitors(self):
+        return self.env['muki.rest'].browse(self._context.get('active_ids'))
+
+    # session_ids = fields.Many2many('muki.rest',
+    #     string="Sessions", required=True, default=_default_sessions)
+    # attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    @api.multi
     def create_visitor(self):
-        active_ids = self._context.get('active_ids', []) or []
-        for record in self.env['muki.rest'].browse(active_ids):
-            record.visitor_name = self.visitor_name
+        customer = self.env['res.partner']
+        customers = customer
+        for visitor_vals in self._get_visitors():
+            customers += customer.create(visitor_vals)
+        customers.post()
+
+        action_vals = {
+            'name': _('customers'),
+            'domain': [('id', 'in', customers.ids)],
+            'view_type': 'form',
+            'res_model': 'res.partner',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+        }
+        return action_vals
+
+    
