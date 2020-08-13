b6eb205497715bcf7b80bbac5246fd60c65b12ac
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-13 14:11:12 -0500

    TEST LOGIN INFO DATA

--- template_module/models/muki_rest.py
+++ template_module/models/muki_rest.py
@@ -45,6 +45,8 @@ class MukiREST(models.Model):
 				'X-VSO-caller': ipBase}
 
 		datas = http.request('POST', url, fields=values, headers=headers)
+		logging.info("DATA: " + str(datas))
+
 		datas = json.loads(datas.data.decode('utf-8'))
 		logging.info("VALOR: " + str(datas))
 		# data = urllib.parse.urlencode(values).encode('utf-8')
