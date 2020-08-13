351218ce8b09b6e5e4760f550630791c77ce680b
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-13 14:37:38 -0500

    test

--- template_module/models/muki_rest.py
+++ template_module/models/muki_rest.py
@@ -28,7 +28,7 @@ class MukiREST(models.Model):
 	visitor = fields.Char("Visitor ID")
 
 
-	def search_visitor(self):
+	def search_visitor(self, hstatus, fname):
 		ip_address = '190.140.165.45'
 		#CONVIRTIENDO A FORMATO ASCII EL IP
 		ip_address_bytes = ip_address.encode('ascii')
@@ -38,7 +38,7 @@ class MukiREST(models.Model):
 		http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 		url = 'https://visitors.stri.si.edu/services/getVisits'
 
-		values = {"status": "hstate","name": "fname"}
+		values = {"status": hstatus,"name": fname}
 		logging.info("VALUES: " + str(values))
 	
 		headers={'Accept': 'application/json',
@@ -50,14 +50,15 @@ class MukiREST(models.Model):
 		datas = json.loads(datas.data.decode('utf-8'))
 		# data = urllib.parse.urlencode(values).encode('utf-8')
 		# declaramos los headers necesarios
-		for data in datas:
-		   self.env['muki.rest'].create({
-			   'visitor': data['visitor_id'],
-			   'nombre': data['visitor_name'],
-			   'fname': data['name'],
-			   'lname': data['last_name'],
-			   'visitor_email': data['email'],
-			   'hstatus': data['status']
-		   })
-		   logging.info("CONTENIDO: " + str(datas))
-		# print(datas)
+	
+		# for data in datas:
+		#    self.env['muki.rest'].create({
+		# 	   'visitor': data['visitor_id'],
+		# 	   'nombre': data['visitor_name'],
+		# 	   'fname': data['name'],
+		# 	   'lname': data['last_name'],
+		# 	   'visitor_email': data['email'],
+		# 	   'hstatus': data['status']
+		#    })
+		#    logging.info("CONTENIDO: " + str(datas))
+		print(datas)
