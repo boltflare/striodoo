c44d44ae8719529568076aeb4a0b83f097f8ae75
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-19 17:15:49 -0500

    update delete for create

--- template_module/wizard/create_customer.py
+++ template_module/wizard/create_customer.py
@@ -5,24 +5,24 @@ from odoo import models, fields, api, _
 # _logger = logging.getLogger(__name__)
 
 class CreateCustomer(models.TransientModel):
-    _name = 'create.customer'
-    _description = 'Create a new visitor on customers'
-    
-    nombre = fields.Char("Name")
-    fname = fields.Char("First Name")
-    lname = fields.Char("Last Name")
-    visitor_email = fields.Char("Email")
-    hvisit = fields.Char("Visitor ID")
+	_name = 'create.customer'
+	_description = 'Create a new visitor on customers'
+	
+	nombre = fields.Char("Name")
+	fname = fields.Char("First Name")
+	lname = fields.Char("Last Name")
+	visitor_email = fields.Char("Email")
+	hvisit = fields.Char("Visitor ID")
  
-    #EN ESTE METODO SE OBTIENE LOS REGISTROS CON EL CHECK ACTIVO, RECORRO LA VISTA Y CREO EN CLIENTES EL REGITRO SELECCIONADO
-    def create_customer(self):
-        active_ids = self._context.get('active_ids', []) or []
-        for record in self.env['muki.rest'].browse(active_ids):
-            # record.nombre = self.env['muki.rest'].search([('nombre', '=', self.nombre)])
-            self.env["res.partner"].create({'name':record.nombre,'visitor':record.hvisit,'email':record.visitor_email})
-            
-		self.env['muki.rest'].search([()]).unlink() 
-            
-        
+	#EN ESTE METODO SE OBTIENE LOS REGISTROS CON EL CHECK ACTIVO, RECORRO LA VISTA Y CREO EN CLIENTES EL REGITRO SELECCIONADO
+	def create_customer(self):
+		active_ids = self._context.get('active_ids', []) or []
+		for record in self.env['muki.rest'].browse(active_ids):
+			# record.nombre = self.env['muki.rest'].search([('nombre', '=', self.nombre)])
+			self.env["res.partner"].create({'name':record.nombre,'visitor':record.hvisit,'email':record.visitor_email})
+			
+		self.env['muki.rest'].search([()]).unlink()
+			
+		
    
-    
+	
