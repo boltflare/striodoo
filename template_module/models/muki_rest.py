fd327e30f047a9d5e71cb2e934878028e5e7bcf9
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-19 11:00:41 -0500

    test 11

--- template_module/models/muki_rest.py
+++ template_module/models/muki_rest.py
@@ -74,11 +74,11 @@ class MukiREST(models.Model):
 
 	#ESTOY AGREGANDO LA FUNCION PARA CREAR EL CUSTOMER AQUI
 	def create_customer(self):
-        # context = dict(self._context or {})
-        # active = self.env['muki.rest'].browse(context.get('active_ids'))
-        active_ids = self._context.get('active_ids', []) or []
-        for record in self.env['muki.rest'].browse(active_ids):
-            record.nombre = self.nombre
-            record.visitor_email = self.visitor_email
-            record.hvisit = self.hvisit
-            self.env["res.partner"].create({'name':record.nombre,'email':record.visitor_email, 'visitor':record.hvisit})
+		# context = dict(self._context or {})
+		# active = self.env['muki.rest'].browse(context.get('active_ids'))
+		active_ids = self._context.get('active_ids', []) or []
+		for record in self.env['muki.rest'].browse(active_ids):
+			record.nombre = self.nombre
+			record.visitor_email = self.visitor_email
+			record.hvisit = self.hvisit
+			self.env["res.partner"].create({'name':record.nombre,'email':record.visitor_email, 'visitor':record.hvisit})
