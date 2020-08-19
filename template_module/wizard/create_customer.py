750c048dcd93e29dfbbe89e9eba87fec1656e26f
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-19 15:57:25 -0500

    Prueba de crear customer

--- template_module/wizard/create_customer.py
+++ template_module/wizard/create_customer.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# from . import library2
-# import json
 from odoo import models, fields, api, _
 
 # import logging
@@ -15,50 +13,15 @@ class CreateCustomer(models.TransientModel):
     lname = fields.Char("Last Name")
     visitor_email = fields.Char("Email")
     hvisit = fields.Char("Visitor ID")
-
-    """ def update_state(self):
-        active_ids = self._context.get('active_ids', []) or []
-        for record in self.env['sale.order'].browse(active_ids):
-            record.state = self.state  """   
-
-   
-    
+ 
+    #EN ESTE METODO SE OBTIENE LOS REGISTROS CON EL CHECK ACTIVO, RECORRO LA VISTA Y CREO EN CLIENTES EL REGITRO SELECCIONADO
     def create_customer(self):
-        # context = dict(self._context or {})
-        # active = self.env['muki.rest'].browse(context.get('active_ids'))
         active_ids = self._context.get('active_ids', []) or []
         for record in self.env['muki.rest'].browse(active_ids):
             # record.nombre = self.env['muki.rest'].search([('nombre', '=', self.nombre)])
-            # record.nombre = self.nombre
-            # record.visitor_email = self.visitor_email
-            # record.hvisit = self.hvisit
-            self.env["res.partner"].create({'name':record.nombre})
-            # self.env["res.partner"].create({'name':record.nombre,'email':record.visitor_email, 'visitor':record.hvisit})
-
-"""  def create_visitor(self):
-        # active_ids = self._context.get('active_ids', []) or []
-       
-        api = library2.RestAPI()
-        api.authenticate()
-                
-        # test API
-        logging.info(str(api.execute('/api')))
-        logging.info(str(api.execute('/api/user')))
-
-                
-        #EJEMPLO FUNCIONAL 
-        response = api.execute('/api/custom/create/customer')
-        logging.info(str(response))
-
-        result = response['result']
-        for entry in result:
-            nomb = entry.get('nombre')
-            correo = entry.get('visitor_email')
-            visit = entry.get('hvisit')
-            for record in self.env['muki.rest'].browse(self._context.get('active_ids')):
-                record.create = self.env["res.partner"].create({'name':nomb,'email':correo, 'visitor':visit}) """
+            self.env["res.partner"].create({'name':record.nombre,'visitor':record.hvisit,'email':record.visitor_email})
             
-        # return self.env['muki.rest'].browse(self._context.get('active_ids'))
+
             
         
    
