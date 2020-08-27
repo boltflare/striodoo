a901a6b3d8c6c43754eda84f491758f12745b54d
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-08-27 13:18:10 -0500

    update filter payment

--- template_module/models/res_partner.py
+++ template_module/models/res_partner.py
@@ -32,22 +32,28 @@ class UpdateCustomer(models.Model):
 			nombre = entry.get('name')
 			correo = entry.get('email')
 			visit = entry.get('visitor')
-			self.env["res.partner"].update({'name':nombre,'email':correo, 'visitor':visit})
+			self.env["res.partner"].write({'name':nombre,'email':correo, 'visitor':visit})
 			# self.env["res.partner"].create({'name':number,'hstatus':estado,'email':total,'visitor':visit})
 			logging.info(str(response))
 		
+	""" # FUNCION PARA HACER UPDATE EN ODOO
+	@api.multi
+	def update_status_meal_card(self):
+		lines = self.env['account.invoice.line'].search([('invoice_id', '=', self.id)])
+		if lines:
+			lines.write({'hs_state':self.state}) """
 		
-		# /api/write
-		"""  #EJEMPLO FUNCIONAL 
-		response = api.execute('/api/custom/update/customer')
-		result = response['result']
-		for entry in result:
-			nombre = entry.get('name')
-			correo = entry.get('email')
-			visit = entry.get('visitor')
-			self.env["muki.rest"].create({'visitor_name':nombre,'visitor_email':correo, 'visitor':visit})
-			# self.env["res.partner"].create({'name':number,'hstatus':estado,'email':total,'visitor':visit})
-			logging.info(str(response)) """
+	# /api/write
+	"""  #EJEMPLO FUNCIONAL 
+	response = api.execute('/api/custom/update/customer')
+	result = response['result']
+	for entry in result:
+		nombre = entry.get('name')
+		correo = entry.get('email')
+		visit = entry.get('visitor')
+		self.env["muki.rest"].create({'visitor_name':nombre,'visitor_email':correo, 'visitor':visit})
+		# self.env["res.partner"].create({'name':number,'hstatus':estado,'email':total,'visitor':visit})
+		logging.info(str(response)) """
 		
 
 		#URL: https://demo12.mukit.at/api/write/res.partner?ids=%5B14%5D&values=%7B%27name%27%3A%20%27TEST%20UPDATE%27%7D
