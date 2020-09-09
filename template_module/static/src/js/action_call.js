5b839019922d5a79166ce9e34f0388165a51b674
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-09-09 10:11:31 -0500

    update 4

--- template_module/static/src/js/action_call.js
+++ template_module/static/src/js/action_call.js
@@ -1,43 +1,21 @@
 // alert("TEST1");
-odoo.define('template_module.action_call'), function (require) {
-    "use strict";
-    
-    var rpc = require('web.rpc');
-    var session = require('web.session');
-    var _t = core._t;
-    var core = require('web.core');
-    // var ListView = require('web.ListView');
-    var ListController = require("web.ListController");
+odoo.define('template_module.action_call', function (require) {
 
-    // alert("Va a llamar la funcion");
-    ListController.include({
-        renderButtons: function($node) {
-        this._super.apply(this, arguments);
-            if (this.$buttons) {
-              this.$buttons.find('.oe_action_button').click(this.proxy('action_def'));
+    "use strict";       
+    var core = require('web.core');
+    var ListView = require('web.ListView'); 
+    var ListController = require("web.ListController");
+    
+    var includeDict = {
+        renderButtons: function () {
+            this._super.apply(this, arguments);
+            if (this.modelName === "muki.rest") {
+                var your_btn = this.$buttons.find('button.oe_action_button')
+                your_btn.on('click', this.proxy('oe_action_button'))
             }
         },
-
-        action_def: function () {
-            var self =this
-            var user = session.uid;
-            rpc.query({
-                model: 'muki.rest',
-                method: 'search_visitor',
-                // args: [[user],{'id':user}],
-                }).then(function (e) {
-                    self.do_action({
-                        name: _t('wizard_form_search'),
-                        type: 'ir.actions.act_window',
-                        res_model: 'muki.rest',
-                        views: [[false, 'form']],
-                        view_mode: 'form',
-                        target: 'new',
-                    });
-                    window.location
-                });
-            },
-    
-    
-    });     
-}
+        oe_action_button: function(){
+        }
+    };
+    ListController.include(includeDict);
+    });
