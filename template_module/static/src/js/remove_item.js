d323a2dbf82c30eb127e2ec55b5ea02b5d37abd4
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-09-09 14:34:32 -0500

    update 19

--- template_module/static/src/js/remove_item.js
+++ template_module/static/src/js/remove_item.js
@@ -13,21 +13,21 @@
 //     console.log('Esta funcionando');
 // }
 $( document ).ready(function() {
-    
     $(".oe_action_button").click(function(){
-        remover_item();
-    });
-    
- });
-//document.getElementById("btn_search").onclick = remover_item;
-function remover_item(){
-setTimeout(function(){ 
-
-var list = document.getElementsByClassName("ui-sortable")[0];
-    console.log(list);
-    list.removeChild(list.childNodes[0]);
-    console.log('Esta funcionando');
-}, 10000);
-   
-}
+    setTimeout(function(){ 
+    console.log($("button[name*='search_visitor']"));
+    $("button[name*='search_visitor']").attr("onclick","remover_item()");
+     }, 2000);
+      
+      });
+     });
+    function remover_item(){
+    setTimeout(function(){ 
+    alert("termino");
+     var list = document.getElementsByClassName("ui-sortable")[0];
+        console.log(list);
+        list.removeChild(list.childNodes[0]);
+        console.log('Esta funcionando');
+     }, 12000);
+    }
 
