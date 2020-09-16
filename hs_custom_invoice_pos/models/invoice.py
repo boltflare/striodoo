29f623a42e533e71e135ec718bc82961fa5cc722
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-09-16 15:25:28 -0500

    update 9

--- hs_custom_invoice_pos/models/invoice.py
+++ hs_custom_invoice_pos/models/invoice.py
@@ -12,8 +12,8 @@ class InvoiceInherit2(models.Model):
 	def _compute_journal_id(self):
 		for invoice in self:
 			invoice.hs_journal = invoice.journal_id.name
-			if self.pos_invoice:
-				self.pos_invoice = True
+		if self.pos_invoice:
+			self.pos_invoice = True
 
 	# pos_invoice = fields.Char(compute='_compute_state_invoice', string='POS Invoice', store=True)
 	
