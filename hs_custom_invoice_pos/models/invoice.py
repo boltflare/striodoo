2f4c1e827accbbf775c839db97a0ea8a62fb9f0a
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-09-16 14:15:45 -0500

    update invoice

--- hs_custom_invoice_pos/models/invoice.py
+++ hs_custom_invoice_pos/models/invoice.py
@@ -17,7 +17,7 @@ class InvoiceInherit2(models.Model):
 
 	@api.depends('journal_id') 
 	def _get_state_invoice(self):
-		invoice = self.env['account.invoice'].browse(self.env.journal_id)
+		invoice = self.env['account.invoice'].search([])
 		for fac in self:
 			fac.pos_invoice = True if invoice.journal_id.name == 'POS Sale Journal (USD)' else False
 
