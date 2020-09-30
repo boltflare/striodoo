b39fda702d5b972a84149ab67c7dedfe8bdd01b0
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-09-30 16:47:31 -0500

    update sales report

--- account_invoice_line_report/report/sales_report.py
+++ account_invoice_line_report/report/sales_report.py
@@ -79,11 +79,11 @@ class anexo72Report(models.AbstractModel):
 		dt_from = options['date'].get('date_from')
 		dt_to = options['date'].get('date_to')
 		query = """
-		select CONCAT (cuenta.code,' ',cuenta.name),
+		select CONCAT ('[',cuenta.code,']',' ',cuenta.name),
 		cliente.name,
 		sale.name,
 		categ.complete_name, 
-		CONCAT (item.default_code,' ',tem.name),
+		CONCAT ('[',item.default_code,']',' ',tem.name),
 		case when (invoice.type = 'out_invoice') then detalle.quantity
 		else (detalle.quantity*-1) end AS quantity,
 		case when (invoice.type = 'out_invoice') then detalle.price_unit
@@ -95,7 +95,7 @@ class anexo72Report(models.AbstractModel):
 		CONCAT (cuenta.stri_fund,',', cuenta.stri_budget,',',cuenta.stri_desig,',',cuenta.stri_dept,',',cuenta.stri_account,',',cuenta.stri_class,',',cuenta.stri_program,',',cuenta.stri_project,',',cuenta.stri_activity,',',cuenta.stri_type) as chartfield
 		from account_invoice_line As detalle , account_invoice as invoice, res_partner as cliente, account_account as cuenta, res_partner as sale, res_users as ur, product_product as item, product_template as tem, product_category as categ
 		where (invoice.date_invoice BETWEEN '{}' AND '{}') and detalle.invoice_id = invoice.id and invoice.partner_id = cliente.id and detalle.account_id = cuenta.id and invoice.user_id = ur.id and ur.partner_id = sale.id
-		and invoice.type IN('out_invoice','out_refund') and invoice.state <> 'draft' and detalle.product_id = item.id and item.product_tmpl_id = tem.id and tem.categ_id = categ.id;
+		and invoice.type IN('out_invoice','out_refund') and invoice.state NOT IN ('draft','cancel') and detalle.product_id = item.id and item.product_tmpl_id = tem.id and tem.categ_id = categ.id;
 		""".format(dt_from, dt_to)
 
 		self.env.cr.execute(query)
