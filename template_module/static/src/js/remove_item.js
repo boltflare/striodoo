f474b7cbd61d2143cf8955ddcd0919409db6b2e8
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-10-08 14:06:34 -0500

    test 2 js

--- template_module/static/src/js/remove_item.js
+++ template_module/static/src/js/remove_item.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
    alert("TEST1");
       remover_item();
+   $(".oe_action_button").click(function(){
+
+         setTimeout(function(){ 
+         console.log($("button[name*='search_visitor']"));
+         $("button[name*='search_visitor']").attr("onclick","remover_item()");
+          }, 4000);
+           
+           });
    });
     
 //  });
@@ -11,14 +19,7 @@ $( document ).ready(function() {
 //     list.removeChild(list.childNodes[0]);
 //     console.log('Esta funcionando');
 // }
-$(".oe_action_button").click(function(){
 
-    setTimeout(function(){ 
-    console.log($("button[name*='search_visitor']"));
-    $("button[name*='search_visitor']").attr("onclick","remover_item()");
-     }, 4000);
-      
-      });
      
     function remover_item(){
     $("body").append("<div  id='loading_div' style='width: 100%;background-color:black;opacity:0.5;min-height: 100%;height: auto !important;position: fixed;top:0;left:0;'>test</div>")
