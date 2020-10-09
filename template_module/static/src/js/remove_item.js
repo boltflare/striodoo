c2a79b2619d843198a670151c75e15cf8366e1bb
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-10-09 10:53:44 -0500

    update

--- template_module/static/src/js/remove_item.js
+++ template_module/static/src/js/remove_item.js
@@ -1,81 +1,62 @@
 $( document ).ready(function() {
-   // alert("TEST2");
-      // remover_item();
+
+   //console.log("cambio")
+   //console.log("Boton = " + $(".oe_action_button"))
+  // console.log("Boton = " + $(".oe_action_button").length)
    
-      console.log("cambio")
-      console.log("Boton = " + $(".oe_action_button"))
-      console.log("Boton = " + $(".oe_action_button").length)
-      
-      
-      validar_boton();
-   // $(".oe_action_button").click(function(){
-   //    alert("TEST_1234");         
-   //    setTimeout(function(){ 
-   //       console.log($("button[name*='search_visitor']"));
-   //       $("button[name*='search_visitor']").attr("onclick","remover_item()");
-   //        }, 4000);
-           
-   //         });
-   //       }, 4000);
-   });
-    
-//  });
-// //document.getElementById("btn_search").onclick = remover_item;
-// function remover_item(){
-//     var list = document.getElementsByClassName("ui-sortable")[0];
-//     console.log(list);
-//     list.removeChild(list.childNodes[0]);
-//     console.log('Esta funcionando');
-// }
+   
+   validar_boton();
 
-     
-    function remover_item(){
-    $("body").append("<div  id='loading_div' style='width: 100%;background-color:black;opacity:0.5;min-height: 100%;height: auto !important;position: fixed;top:0;left:0;'>test</div>")
-    setTimeout(function(){ 
-      var list = document.getElementsByClassName("ui-sortable")[0];
-      console.log(list)
-    if(list == "")
-    {
-       console.log("no encontrado")
-      remover_item()
-   }
-   else
-   {
-      remover_item_final()
-   }
-     }, 1000);
-    }
+});
 
-    function remover_item_final(){
-      alert("termino");
+ function remover_item(){
+ $("body").append("<div  id='loading_div' style='width: 100%;background-color:black;opacity:0.5;min-height: 100%;height: auto !important;position: fixed;top:0;left:0;'></div>")
+ setTimeout(function(){ 
+   var list = document.getElementsByClassName("ui-sortable")[0];
+  // console.log(list)
+ if(list == "")
+ {
+    console.log("no encontrado")
+   remover_item()
+}
+else
+{
+   remover_item_final()
+}
+  }, 1000);
+ }
+
+ function remover_item_final(){
+  // alert("termino");
+
+   setTimeout(function() {
       $("#loading_div").remove();
-      setTimeout(function() {
-         var list = document.getElementsByClassName("ui-sortable")[0];
-         console.log("lista: " + list);
-         list.removeChild(list.childNodes[0]);
-         console.log('Esta funcionando');
-      }, 1000);
-       
-   }
+      var list = document.getElementsByClassName("ui-sortable")[0];
+     // console.log("lista: " + list);
+      list.removeChild(list.childNodes[0]);
+     // console.log('Esta funcionando');
+   }, 3500);
+    
+}
 
-   function validar_boton() {
-      setTimeout(function(){
-      var boton = $(".oe_action_button").length;
-      if (boton <= 0) {
-         console.log("No se Encuentra!")
-         validar_boton()
-      }
-      else {
-         $(".oe_action_button").attr("onclick","llamar_boton()");
-      }
-      },500)
-      
+function validar_boton() {
+   setTimeout(function(){
+   var boton = $(".oe_action_button").length;
+   if (boton <= 0) {
+      //console.log("No se Encuentra!")
+      validar_boton()
    }
+   else {
+      $(".oe_action_button").attr("onclick","llamar_boton()");
+   }
+   },500)
+   
+}
 
-   function llamar_boton() {
-      alert("TEST_1234");         
-      setTimeout(function(){ 
-         console.log($("button[name*='search_visitor']"));
-         $("button[name*='search_visitor']").attr("onclick","remover_item()");
-      }, 4000);
-   }
+function llamar_boton() {
+   ///alert("TEST_1234");         
+   setTimeout(function(){ 
+      //.log($("button[name*='search_visitor']"));
+      $("button[name*='search_visitor']").attr("onclick","remover_item()");
+   }, 4000);
+}
