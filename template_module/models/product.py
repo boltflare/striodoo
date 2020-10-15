5b586f5742c68565bfa8295cf1e39910317186e6
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-10-15 11:45:06 -0500

    update res_partner

--- template_module/models/product.py
+++ template_module/models/product.py
@@ -5,10 +5,8 @@ from odoo import models, fields, api, exceptions
 class ProductTemplateInherit2(models.Model):
 	_inherit = 'product.template'
 
-	visit_categ =  fields.Selection(string='Visitor Category',
-		selection=[('A', 'A'), ('B', 'B'), ('C', 'C')], 
-		help='Opción para poder clasificar los productos correspondientes a la categoria del visitante')
-
+	visit_categ =  fields.Char(string='Visitor Category')
+		
 
 	#SOBRESCRIBIENDO EL METODO PARA SOLO MOSTRAR EL NAME
 	""" @api.multi
@@ -48,8 +46,6 @@ class ProductInherit1(models.Model):
 	_inherit = 'product.product'
 	
 
-	visit_categ =  fields.Selection(string='Visitor Category',
-		selection=[('A', 'A'), ('B', 'B'), ('C', 'C')], 
-		help='Opción para poder clasificar los productos correspondientes a la categoria del visitante')
+	visit_categ =  fields.Char(string='Visitor Category')
 
 
