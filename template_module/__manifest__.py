9ebbe0e7dc41d020db4d40041cfd1f8812aa5985
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-10-15 13:02:38 -0500

    update vso_search
    Modificaciones a:
    Sea agrego campo Visitor Category en al form del customer y productos.
    Se agrego propiedad que solo sea visible si el customer es visitante

--- template_module/__manifest__.py
+++ template_module/__manifest__.py
@@ -13,9 +13,8 @@
 	'version': '5.0',
 	'license': 'LGPL-3',
 
-
 	# any module necessary for this one to work correctly
-	'depends': ['base', 'account'],
+	'depends': ['base', 'account', 'hs_customer_class_code'],
 
 	'external_dependencies': {
 		'python' : ['requests_oauthlib', 'pprint']
@@ -26,9 +25,9 @@
 		'security/user_rules.xml',
 		'security/ir.model.access.csv',
 		'views/muki_rest.xml',
-		# 'views/res_partner.xml',
+		'views/res_partner.xml',
 		'views/vso_button.xml',
-		# 'views/create_customer_wizard.xml',
+		'views/product_view.xml',
 	],
 	'qweb': [
         "static/src/xml/muki_vso.xml",
