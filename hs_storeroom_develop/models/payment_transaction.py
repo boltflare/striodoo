3ca5e6cbc69cbfaec86105d1266b4319384adc65
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-10-22 15:53:19 -0500

    Update payment_transaction.py

--- hs_storeroom_develop/models/payment_transaction.py
+++ hs_storeroom_develop/models/payment_transaction.py
@@ -9,3 +9,11 @@ _logger = logging.getLogger(__name__)
 
 class TransactionInherit(models.Model):
 	_inherit = "payment.transaction"
+
+	def write(self, vals):
+		if vals.get('partner_country_id'):
+			country = vals.get('partner_country_id')
+			if not country:
+				country = self.env['res.company']._company_default_get().country_id.id
+				vals['partner_country_id'] = country
+		return super(TransactionInherit, self).write(vals);
