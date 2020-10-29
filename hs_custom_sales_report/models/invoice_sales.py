e1e05903155fb58dc9e5961e6db4f5f0e2cdb252
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-10-29 16:19:46 -0500

    Update invoice_sales.py

--- hs_custom_sales_report/models/invoice_sales.py
+++ hs_custom_sales_report/models/invoice_sales.py
@@ -15,10 +15,33 @@ class InvoiceInherit2(models.Model):
 class accountPaymentInherit(models.Model):
 	_inherit = 'account.payment'
 
-	diario  = fields.Char(string='Invoice Journal', related='partner_id.invoice_ids.journal_id.name')
+	# diario  = fields.Char(string='Invoice Journal', related='partner_id.invoice_ids.journal_id.name')
 	# diario = fields.Char(compute='_compute_journal_name', string='Invoice Journal', store=True)
+	invoice_journal_id = fields.Many2one(
+		comodel_name='account.journal', 
+		string="Related Journal", 
+		compute='_compute_journal_name',
+	)
+	diario = fields.Char(string='Invoice Journal', related='invoice_journal_id.name', store=True)
 
-""" 	@api.depends('invoice_ids.hs_journal') 
+	@api.depends('invoice_ids') 
 	def _compute_journal_name(self):
 		for payment in self:
-			payment.diario = payment.invoice_ids.hs_journal """
+			if payment.invoice_ids:
+				for invoice in payment.invoice_ids:
+					payment.invoice_journal_id = invoice.journal_id
+					break
+			else:
+				payment.diario = False
+
+
+	def _search_journal_name(self, operator, value):
+		return [('name', operator, value)]
+
+
+	"""	
+	@api.depends('invoice_ids.hs_journal') 
+	def _compute_journal_name(self):
+		for payment in self:
+			payment.diario = payment.invoice_ids.hs_journal
+	"""
