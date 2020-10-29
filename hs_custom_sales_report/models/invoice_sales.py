7c7e9df4bd8506bfd548c4e2f6df57f29d224934
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-10-29 16:04:31 -0500

    Update invoice_sales.py

--- hs_custom_sales_report/models/invoice_sales.py
+++ hs_custom_sales_report/models/invoice_sales.py
@@ -15,10 +15,55 @@ class InvoiceInherit2(models.Model):
 class accountPaymentInherit(models.Model):
 	_inherit = 'account.payment'
 
-	diario  = fields.Char(string='Invoice Journal', related='partner_id.invoice_ids.journal_id.name')
+	# diario  = fields.Char(string='Invoice Journal', related='partner_id.invoice_ids.journal_id.name')
 	# diario = fields.Char(compute='_compute_journal_name', string='Invoice Journal', store=True)
+	diario = fields.Char(string='Invoice Journal', compute='_compute_journal_name', search='_search_journal_name')
 
-""" 	@api.depends('invoice_ids.hs_journal') 
+	@api.depends('invoice_ids') 
 	def _compute_journal_name(self):
 		for payment in self:
-			payment.diario = payment.invoice_ids.hs_journal """
+			if payment.invoice_ids:
+				for invoice in payment.invoice_ids:
+					payment.diario = invoice.journal_id.name
+					break
+			else:
+				payment.diario = False
+
+
+	def _search_journal_name(self, operator, value):
+		return [('name', operator, value)]
+
+
+	"""	
+	@api.depends('invoice_ids.hs_journal') 
+	def _compute_journal_name(self):
+		for payment in self:
+			payment.diario = payment.invoice_ids.hs_journal
+	"""
+
+
+
+	@api.multi
+    # do not depend on 'sequence_id.date_range_ids', because
+    # sequence_id._get_current_sequence() may invalidate it!
+    @api.depends('sequence_id.use_date_range', 'sequence_id.number_next_actual')
+    def _compute_seq_number_next(self):
+        '''Compute 'sequence_number_next' according to the current sequence in use,
+        an ir.sequence or an ir.sequence.date_range.
+        '''
+        for journal in self:
+            if journal.sequence_id:
+                sequence = journal.sequence_id._get_current_sequence()
+                journal.sequence_number_next = sequence.number_next_actual
+            else:
+                journal.sequence_number_next = 1
+
+
+	@api.multi
+    def _inverse_seq_number_next(self):
+        '''Inverse 'sequence_number_next' to edit the current sequence next number.
+        '''
+        for journal in self:
+            if journal.sequence_id and journal.sequence_number_next:
+                sequence = journal.sequence_id._get_current_sequence()
+                sequence.sudo().number_next = journal.sequence_number_next
