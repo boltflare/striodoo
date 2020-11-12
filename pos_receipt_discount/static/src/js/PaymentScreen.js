8f04ccba6882f21d05eb1048fbda79d297265c6d
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-11-12 10:53:46 -0500

    update pos_receipt

---
+++ pos_receipt_discount/static/src/js/PaymentScreen.js
@@ -0,0 +1,19 @@
+odoo.define('pos_receipt_discount.PaymentScreen', function(require) {
+	'use strict';
+	console.log("Ento en el archivo")
+	
+	const PaymentScreen = require('point_of_sale.PaymentScreen');
+	const Registries = require('point_of_sale.Registries');
+
+
+	const InvoiceButtonPaymentScreen = (PaymentScreen) =>
+		class extends PaymentScreen {
+		constructor() {
+			super(...arguments);
+			this.toggleIsToInvoice();
+		}
+	}
+
+	Registries.Component.extend(PaymentScreen, InvoiceButtonPaymentScreen);
+	return InvoiceButtonPaymentScreen;
+});
