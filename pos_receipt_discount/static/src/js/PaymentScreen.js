e3c50b529f8b89f82f415696eea32cd153edd0e5
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-11-13 08:54:31 -0500

    install pos_receipt_discount

---
+++ pos_receipt_discount/static/src/js/PaymentScreen.js
@@ -0,0 +1,30 @@
+// window.onload = function() {
+// 	document.getElementsByClassName("js_invoice").click();
+// }
+odoo.define('pos_receipt_discount.PaymentScreen', function(require) {
+alert('test');
+const boton = document.getElementsByClassName("pay");
+
+boton.onclick = function(){
+	alert('click');
+}
+});
+/* odoo.define('pos_receipt_discount.PaymentScreen', function(require) {
+	'use strict';
+	console.log("Ento en el archivo")
+	
+	const PaymentScreen = require('point_of_sale.PaymentScreenWidget');
+	const Registries = require('point_of_sale.Registries');
+
+
+	const InvoiceButtonPaymentScreen = (PaymentScreen) =>
+		class extends PaymentScreen {
+		constructor() {
+			super(...arguments);
+			this.toggleIsToInvoice();
+		}
+	}
+
+	Registries.Component.extend(PaymentScreen, InvoiceButtonPaymentScreen);
+	return InvoiceButtonPaymentScreen;
+}); */
