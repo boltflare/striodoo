a7a3d12a8657db523cb3e5cc72273eaf85994d7d
CeilaHernandez10 <chernandez@hermecsolutions.com>
2020-11-25 15:59:22 -0500

    Update vso Integration
    Se realizaron ajustes para validar si no hay ningun resultado, se corrigio que no se muestre form al hacer click a una fila, se corrigio que no se agregue una linea de mas cuando se hace la busqueda.

--- template_module/models/muki_rest.py
+++ template_module/models/muki_rest.py
@@ -7,6 +7,7 @@ import socket
 import logging
 _logger = logging.getLogger(__name__)
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 class MukiREST(models.Model):
 	_name = "muki.rest"
@@ -15,6 +16,23 @@ class MukiREST(models.Model):
 	user_id = fields.Many2one('res.users', string="Current User",
 		default=lambda self: self.env.user)
 
+	#CAMPOS WIZARD 
+	hvisit = fields.Char("Visitor ID")
+	whstatus = fields.Selection([
+		('Check-OUT', 'Check-OUT'),
+		('Cancelled', 'Cancelled'),
+		('Declined', 'Declined'),
+		('Draft', 'Draft'),
+		('Revision', 'Revision'),
+		('Check-IN', 'Check-IN'),
+		('Approved', 'Approved'),
+		('Submit', 'Submit')],string = 'Status')
+	wnombre = fields.Char("Name")
+	wfname = fields.Char("First Name")
+	wlname = fields.Char("Last Name")
+	wvisitor_email = fields.Char("Email")
+	
+	#CAMPOS VISTA
 	hstatus = fields.Selection([
 		('Check-OUT', 'Check-OUT'),
 		('Cancelled', 'Cancelled'),
@@ -28,7 +46,7 @@ class MukiREST(models.Model):
 	fname = fields.Char("First Name")
 	lname = fields.Char("Last Name")
 	visitor_email = fields.Char("Email")
-	hvisit = fields.Char("Visitor ID")
+	hvisit2 = fields.Char("Visitor ID")
 	hstreet = fields.Char("Street")
 	hstreet2 = fields.Char("Street2")
 	hcity = fields.Char("City")
@@ -36,6 +54,67 @@ class MukiREST(models.Model):
 	hcountry = fields.Char("Country")
 	hcateg = fields.Char("Visitor Category")
 
+	#FUNCION PARA HACER REPLACE DE VALORES NONE
+	def replace(self,data, search, replacement, parent=None, index=None):
+		if data == search:
+			parent[index] = replacement
+		elif isinstance(data, (list, dict)):
+			for index, item in enumerate(data) if isinstance(data, list) else data.items():
+				self.replace(item, search, replacement, parent=data, index=index)
+
+	#FUNCION PARA HACER VALIDACION DE LOS KEYS DENTRO DE ADDRESS
+	def verify_keys(self,address):
+		if {'line1', 'line2','country', 'city', 'zip'} <= address.keys():
+			return True
+		else:
+			return False
+	
+	#FUNCION PARA AGREGAR VALORES A LA VISTA DE RESULTADOS
+	def add_visitor(self, datas):
+		logging.info("Se esta llamando la funcion:")
+		for value in datas.values():
+			for data in value:
+				self.hvisit2= data['user_id']
+				self.nombre= data['visitor_name']
+				self.fname= data['first_name']
+				self.lname= data['last_name']
+				self.visitor_email=data['email']
+				self.hstatus=data['status']
+				self.hcateg=data['visitor_category']
+				self.address = data['funding']['address']
+				# logging.info("ADDRESS: " + str(address))
+				if self.address != '':
+					if self.verify_keys(self.address):
+						self.hstreet=self.address['line1']
+						self.hstreet2=self.address['line2']
+						self.hcity=self.address['city']
+						self.hzip=self.address['zip']
+						self.hcountry=self.address['country']
+						# logging.info("DENTRO DEL IF: " + str(address))
+				else:
+					self.hstreet=""
+					self.hstreet2=""
+					self.hcity=""
+					self.hzip=""
+					self.hcountry=""
+					# logging.info("DENTRO DEL ELSE: " + str(address))
+		
+		self.env['muki.rest'].create({
+			'hvisit2': self.hvisit2,
+			'nombre': self.nombre,
+			'fname': self.fname,
+			'lname': self.lname,
+			'visitor_email': self.visitor_email,
+			'hstatus': self.hstatus,
+			'hcateg':self.hcateg,
+			'hstreet':self.hstreet,
+			'hstreet2':self.hstreet2,
+			'hcity':self.hcity,
+			'hzip':self.hzip,
+			'hcountry':self.hcountry,
+			})
+
+	#FUNCION PARA HACER REQUEST Y OBTENER JSON CON LOS RESULTADOS DE LA BUSQUEDA
 	def search_visitor(self):
 		ip_address = '35.222.101.46'
 		#CONVIRTIENDO A FORMATO ASCII EL IP
@@ -47,82 +126,37 @@ class MukiREST(models.Model):
 		
 		values = dict()
 		filtros_dic = {
-			"visitor_id":self.hvisit,
-			"status": self.hstatus,
-			"visitor_name":self.nombre,
-			"name":self.fname,
-			"last_name":self.lname,
-			"email":self.visitor_email}
+			"visitor_id":self.hvisit2,
+			"status": self.whstatus,
+			"visitor_name":self.wnombre,
+			"name":self.wfname,
+			"last_name":self.wlname,
+			"email":self.wvisitor_email}
 
 		for key, value in filtros_dic.items():
 			if (value != False):
 				values[key] = value
-		# values = {"visitor_id":self.hvisit,"status": self.hstatus,"visitor_name":self.nombre}
 		logging.info("VALUES: " + str(values))
 	
 		headers={'Accept': 'application/json',
 				'X-VSO-caller': ipBase}
 
 		datas = http.request('POST', url, fields=values, headers=headers)
-
-		datas = json.loads(datas.data.decode('utf-8'))
-		logging.info("CONTENIDO: " + str(datas))
-
-		"""for data in datas['visit']:
-			self.env['muki.rest'].create({
-				'hvisit': data['user_id'],
-				'nombre': data['visitor_name'],
-				'fname': data['first_name'],
-				'lname': data['last_name'],
-				'visitor_email': data['email'],
-				'hstatus': data['status'],
-				'hstreet':data['funding']['address']['line1'],
-				'hstreet2':data['funding']['address']['line2'],
-				'hcity':data['funding']['address']['city'],
-				'hzip':data['funding']['address']['zip']
-				# 'hstreet':data['line1'],
-				# 'hstreet2':data['line2'],
-				# 'hcity': data['city'],
-				# 'hzip': data['zip']
-			}) """
-		for data in datas['visit']:
-			hvisit= data['user_id']
-			nombre= data['visitor_name']
-			fname= data['first_name']
-			lname= data['last_name']
-			visitor_email=data['email']
-			hstatus=data['status']
-			hcateg=data['visitor_category']
-			address = data['funding']['address']
-			if address is not None:
-				hstreet=data['funding']['address']['line1']
-				hstreet2=data['funding']['address']['line2']
-				hcity=data['funding']['address']['city']
-				hzip=data['funding']['address']['zip']
-				hcountry=data['funding']['address']['country']
-			else:
-				hstreet=""
-				hstreet2=""
-				hcity=""
-				hzip=""
-				hcountry=""
-			self.env['muki.rest'].create({
-				'hvisit': hvisit,
-				'nombre': nombre,
-				'fname': fname,
-				'lname': lname,
-				'visitor_email': visitor_email,
-				'hstatus': hstatus,
-				'hcateg':hcateg,
-				'hstreet':hstreet,
-				'hstreet2':hstreet2,
-				'hcity':hcity,
-				'hzip':hzip,
-				'hcountry':hcountry,
-				})
-
+		#MOSTRAR EXCEPCION EN CASO DE NO OBTENER RESULTADO
+		try:
+			datas = json.loads(datas.data.decode('utf-8'))
+			#REEMPLAZANDO VALORES VACIOS POR EL STRING ''
+			self.replace(datas, None, '')
+			self.replace(datas, [], '')
+			logging.info("CONTENIDO: " + str(datas.values()))
+			self.add_visitor(datas)
+		except Exception:
+			raise UserError(_('No se han encontrado resultados!'))
+			# raise RedirectWarning("No se han encontrado resultados!", action.id, _('OK'))
+		
 		action = self.env.ref('template_module.muki_rest_action').read()[0]
 		action['target'] = 'main'
-		return action	
+		return action
+
 		# print(datas)
 
