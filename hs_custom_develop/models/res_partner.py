b2aaf1face4998c07427bb6866bbbedc9f28b5ef
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-12-02 17:38:34 -0500

    update readonly customer

--- hs_custom_develop/models/res_partner.py
+++ hs_custom_develop/models/res_partner.py
@@ -10,6 +10,20 @@ class ResPartnerInherit(models.Model):
 	_inherit = "res.partner"
 
 
+	extra_option = fields.Boolean("Display Extra Option",
+		compute="_compute_extra_option")
+
+
+	def _compute_extra_option(self):
+		for partner in self:
+			group = self.env.ref('base.group_partner_manager')
+			user = self.env.user
+			if user in group.users:
+				self.extra_option = True
+			else:
+				self.extra_option = False
+
+
 	@api.multi
 	def write(self, values):
 		"""Sobreescribimos el metodo write para poner por default en
