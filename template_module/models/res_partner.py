5f8802c7630c919f552d71d14f3ead2d0a5b2191
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-12-02 15:29:16 -0500

    update template module

--- template_module/models/res_partner.py
+++ template_module/models/res_partner.py
@@ -1,70 +1,17 @@
-from . import library
-import json
-from odoo import api, fields, models
-import logging
-_logger = logging.getLogger(__name__)
 
-class UpdateCustomer(models.Model):
+from odoo import api, fields, models
+
+class resPartnerInherit2(models.Model):
 	_inherit = 'res.partner'
 
-	def action_update_customer(self):
-		# init API
-		api = library.RestAPI()
-		api.authenticate()
+	visit_category = fields.Char(string='Visitor Category')
 
-		# test API
-		logging.info(str(api.execute('/api')))
-		logging.info(str(api.execute('/api/user')))
-
-
-		data = {
-			'model': "res.partner",
-			'domain': json.dumps([['visitor', '=', '17']]),
-			# 'limit': 1
-		}
-		response = api.execute('/api/search', data=data)
-		logging.info(str(response))
-		
-		
-		# Update customer
-		""" values = {
-			
-		 	'email': "aguila5@comcast.net",
-		}
-		data = {
-		  	'model': "res.partner",
-	 		'domain': json.dumps([['visitor', '=', '17']]),
-		 	'values': json.dumps(values),
-			'fields': json.dumps(['name','email','phone','street','visitor']),
-		} """
-		""" response = api.execute('/api/custom/update/customer', type="GET")
-		result = response['result']
-		for entry in result:
-			nombre = entry.get('name')
-			correo = entry.get('email')
-			visit = entry.get('visitor')
-			self.env["res.partner"].write({'name':nombre,'email':correo, 'visitor':visit})
-			# self.env["res.partner"].create({'name':number,'hstatus':estado,'email':total,'visitor':visit})
-			logging.info(str(response)) """
-		
-	""" # FUNCION PARA HACER UPDATE EN ODOO
-	@api.multi
-	def update_status_meal_card(self):
-		lines = self.env['account.invoice.line'].search([('invoice_id', '=', self.id)])
-		if lines:
-			lines.write({'hs_state':self.state}) """
-		
-	# /api/write
-	"""  #EJEMPLO FUNCIONAL 
-	response = api.execute('/api/custom/update/customer')
-	result = response['result']
-	for entry in result:
-		nombre = entry.get('name')
-		correo = entry.get('email')
-		visit = entry.get('visitor')
-		self.env["muki.rest"].create({'visitor_name':nombre,'visitor_email':correo, 'visitor':visit})
-		# self.env["res.partner"].create({'name':number,'hstatus':estado,'email':total,'visitor':visit})
-		logging.info(str(response)) """
-		
-
-		#URL: https://demo12.mukit.at/api/write/res.partner?ids=%5B14%5D&values=%7B%27name%27%3A%20%27TEST%20UPDATE%27%7D
+	#ESTE CAMPO ES PARA OBTENER EL USUARIO LOGGEADO
+	login = fields.Boolean(string="Is_login", compute="_get_current_user")
+	
+	def _get_current_user(self):
+		#user = self.env['res.users'].browse(self.env.uid)
+		user = self.env.user
+		for sesion in self:
+			sesion.login = True if user.has_group('account.group_account_manager') and user.has_group('account.group_account_user')  else False
+	
--- template_module/wizard/create_customer.py
+++ template_module/wizard/create_customer.py
@@ -1,30 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 
-# import logging
-# _logger = logging.getLogger(__name__)
-
 class CreateCustomer(models.TransientModel):
 	_name = 'create.customer'
 	_description = 'Create a new visitor on customers'
-	
-	nombre = fields.Char("Name")
-	fname = fields.Char("First Name")
-	lname = fields.Char("Last Name")
-	visitor_email = fields.Char("Email")
-	hvisit = fields.Char("Visitor ID")
-	hstreet = fields.Char("Street")
-	hstreet2 = fields.Char("Street2")
-	hcity = fields.Char("City")
-	hzip = fields.Char("Zip")
-	hphone = fields.Char("Phone")
-	hmobile = fields.Char("Mobile")
- 
+
 	#EN ESTE METODO SE OBTIENE LOS REGISTROS CON EL CHECK ACTIVO, RECORRO LA VISTA Y CREO EN CLIENTES EL REGITRO SELECCIONADO
 	def create_customer(self):
 		active_ids = self._context.get('active_ids', []) or []
 		for record in self.env['muki.rest'].browse(active_ids):
-			self.env["res.partner"].create({'name':record.nombre,'visitor':record.hvisit,'street':record.hstreet,'street2':record.hstreet2,'city':record.hcity,'zip':record.hzip,'email':record.visitor_email})
+			self.env["res.partner"].create({'name':record.nombre,'visitor':record.hvisit2,'street':record.hstreet,'street2':record.hstreet2,'city':record.hcity,'zip':record.hzip,'visit_category':record.hcateg,'email':record.visitor_email})
 
 		# ESTA PROPIEDAD PERMITE ELIMINAR TODOS LOS REGISTROS LUEGO DE HACER EL CREATE
 		record_set = self.env['muki.rest'].search([])
